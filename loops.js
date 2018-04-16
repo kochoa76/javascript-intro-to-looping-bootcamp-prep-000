ac382dc07c464ae82a8c9cf7c636e00f784f1be6
ochoa.kayla@yahoo.com <ochoa.kayla@yahoo.com>
2018-04-16 21:19:52 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -20,7 +20,10 @@ function maybeTrue() {
   return Math.random() >= 0.5
 }
 
-function doWhileLoop(array) {
+function doWhileLoop(array) {function maybeTrue() {
+  return Math.random() >= 0.5
+}
+
   do {array.pop()}
   while (array.length > 0 && maybeTrue());
 }
